c9e260aafc063c0d7179bcfca0b7af3f54c2f601
navyasri165 <cnavyasri442@gmail.com>
2026-06-12 12:03:45 +0530

    manupulation

---
+++ manipulation.js
@@ -0,0 +1,14 @@
+/*let value=document.getElementsByClassName("wishes")[0].innerText;
+console.log(value);*/
+let pen=document.getElementByClassName("wishes");
+console.log(pen);
+let mouse=document.getElementByClassName("wishes");
+console.log(mouse);
+let cable=document.getElementByTagName("div");
+console.log(cable);
+let water=document.getElementByquerySelector("div");
+console.log(water);
+let juice=document.getElementByquerySelectorAll("p");
+console.log(juice);
+
+
---
+++ script.js
@@ -0,0 +1,46 @@
+let name="navya";
+var age=20;
+const height="3.5";
+console.log(name);
+console.log(age); 
+console.log(age+5);
+console.log(age/5);
+let food="biryani";
+let breakfast="dosa";
+let food3="idly";
+let food4="pongal";
+if(food==breakfast){
+    console.log("it is nice to have biryani");
+}
+else if(food4 == breakfast)
+{
+    console.log("it is nice to have pongal");
+}
+for(let i=1;i<=10;i++){
+
+    for(let j=1;j<=10;j++){
+        console.log(i+"*" + j+"="+i*j);
+    }
+}
+function cook(){
+    console.log("pour the dosa batter into a bowl");
+    console.log("pour the dosa batter on the pan");
+    console.log("make the dosa round"); 
+    console.log("spill the oil aroubd it");
+    console.log("flip the dosa"); 
+}
+cook()
+function dishwash( vessels){
+console.log("we have to wash"+vessels+"vessels");
+function guess(n){
+    console.log("try a small number.")}
+}
+    function num(n){
+        if(n>10){
+            return;
+        }
+         console.log(n);
+            n+=1;
+            num(n);
+    } 
+num(1);  
